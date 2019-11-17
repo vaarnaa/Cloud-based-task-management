dbc86fbe6f35134b791603d5284888b2b43d9b16
Antero Vaarnamo <antero.vaarnamo@gmail.com>
2019-11-17 19:13:39 +0200

    Added mock login and sign up pages

---
+++ frontend/TaskApplication/app/src/main/java/com/example/taskapplication/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.taskapplication
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.app.Activity
+import android.content.Intent
+import android.provider.AlarmClock.EXTRA_MESSAGE
+
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin) as Button
+        val buttonSignUp = findViewById<Button>(R.id.buttonSignUp) as Button
+        val etUsername = findViewById<EditText>(R.id.et_username) as EditText
+        val etPassword = findViewById<EditText>(R.id.et_password) as EditText
+
+        buttonLogin.setOnClickListener {
+            val username = etUsername.text
+            val password = etPassword.text
+
+            // code to resolve login
+            // afterwards redirect user to some other activity
+        }
+
+        buttonSignUp.setOnClickListener {
+            // redirect user to sign up activity
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // all views
+    var myButton = findViewById(R.id.my_button)
+    var etUsername: EditText = findViewById<EditText>(R.id.et_username) as EditText
+    var etPassword: EditText = findViewById<EditText>(R.id.et_password) as EditText
+    val buttonLogin : Button = findViewById<Button>(R.id.buttonLogin) as Button
+    val buttonSignUp: Button = findViewById<Button>(R.id.buttonSignUp) as Button
+
+    buttonLogin.setOnClickListener(object : View.OnClickListener{
+        override fun onClick(v: View?) {
+            //Your code here
+        }})
+
+    val clickListener = View.OnClickListener { view ->
+
+        when (view.getId()) {
+            R.id.buttonLogin -> resolveLogin()
+            R.id.buttonSignUp -> resolveSignUp()
+        }
+    }
+
+    fun resolveLogin() {
+        val username = et_username.text
+        val password = et_password.text
+
+        // code for resolving login
+        println("login")
+    }
+
+    fun resolveSignUp() {
+        // user directed to signUp view
+
+        println("signUp")
+    }
+    /*
+    // set on-click listener
+    buttonLogin.setOnClickListener {
+        val user_name = et_username.text;
+        val password = et_password.text;
+
+        // your code to validate the user_name and password combination
+        // and verify the same
+
+    }
+    */
+
+*/
+
+
+
+
---
+++ frontend/TaskApplication/app/src/main/java/com/example/taskapplication/SignUpActivity.kt
@@ -0,0 +1,28 @@
+package com.example.taskapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class SignUpActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit) as Button
+        val etUsername = findViewById<EditText>(R.id.et_username) as EditText
+        val etPassword = findViewById<EditText>(R.id.et_password) as EditText
+        val etEmail = findViewById<EditText>(R.id.et_email) as EditText
+
+        buttonSubmit.setOnClickListener {
+            val username = etUsername.text
+            val password = etPassword.text
+            val email = etEmail.text
+
+            // code to resolve sign up
+            // afterwards redirect user to some other activity
+        }
+    }
+}
