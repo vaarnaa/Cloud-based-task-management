58d7a4416354a389b2f7a02ebc50c4ee1b8d50f1
Antero <antero.vaarnamo@aalto.fi>
2019-11-21 23:57:08 +0200

    Extra functionalities added to create project activity

--- frontend/TaskApplication/app/src/main/java/com/example/taskapplication/CreateProjectActivity.kt
+++ frontend/TaskApplication/app/src/main/java/com/example/taskapplication/CreateProjectActivity.kt
@@ -1,22 +1,45 @@
 package com.example.taskapplication
 
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_create_project.*
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CreateProjectActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var cal: Calendar
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_project)
         setSupportActionBar(findViewById(R.id.toolbar))
         buttonAddIcon.setOnClickListener(this)
+        setDeadlineButton.setOnClickListener(this)
+        val textViewDeadline: TextView = findViewById(R.id.tv_project_deadline_time)
+        //textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+        cal = Calendar.getInstance()
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textViewDeadline.text = sdf.format(cal.time)
+
+        }
     }
 
     private fun pickImageFromGallery() {
@@ -34,7 +57,15 @@ class CreateProjectActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.id) {
             R.id.buttonAddIcon -> pickImageFromGallery()
+            R.id.setDeadlineButton -> pickDeadline()
         }
+    }
+
+    private fun pickDeadline() {
+        DatePickerDialog(this, dateSetListener,
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)).show()
     }
 
     public override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
